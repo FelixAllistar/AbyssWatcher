eed17b6ef7ec8a986681e0e9a2584f55d0358273
Felix Allistar <147667330+FelixAllistar@users.noreply.github.com>
2025-12-31 18:02:57 -0500

    feat(backend): Add tauri-plugin-dialog dependency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -78,6 +78,7 @@ pub fn run() {
                         .build(),
                 )?;
             }
+            app.handle().plugin(tauri_plugin_dialog::init())?;
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![get_available_characters, toggle_tracking])
